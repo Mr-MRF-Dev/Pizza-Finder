fa3d5a875b86f1bb5f7ccecda7be81153a4de78c
Amier11ac <shuowala@gmail.com>
2024-01-31 18:35:13 +0330

    added name to Area

--- Area.h
+++ Area.h
@@ -2,15 +2,17 @@
 #ifndef AREA_H
 #define AREA_H
 #include "Location.h"
+#include <string>
+using namespace std;
 
 class Area{
 	Point P1;
 	Point P2;
-	Point P3;
-	Point P4;
+	string nameArea;
 public:
-
-
+	Area(int a1, int a2, int b1, int b2, string name);
+	bool checkInArea(int x, int y);
+	string getName();
 
 };
 
