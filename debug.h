c806c99c4e27317105066de7540279c4f7796015
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-02-03 03:04:16 +0330

    Update debug.h

--- debug.h
+++ debug.h
@@ -12,28 +12,28 @@ using namespace std;
 
 #if DEBUG
 
-    #define log(X, Y)                                                     \
+    #define log2(X, Y)                                                     \
         cout << Color_Gray << "LOG >>> " << X << ": " << Y << Color_Reset \
              << endl;
 
-    #define log(X, Y, Z)                                             \
-        cout << Color_Gray << "LOG >>> " << X << ": " << Y ", " << Z \
+    #define log3(X, Y, Z)                                                \
+        cout << Color_Gray << "LOG >>> " << X << ": " << Y << ", " << Z \
              << Color_Reset << endl;
 
-    #define Err(X, Y)                                                    \
+    #define Err2(X, Y)                                                    \
         cout << Color_Red << "Err >>> " << Color_Reset << X << ": " << Y \
              << endl;
 
-    #define Err(X, Y, Z)                                                      \
+    #define Err3(X, Y, Z)                                                      \
         cout << Color_Red << "Err >>> " << Color_Reset << X << ": " << Y ", " \
              << Z << endl;
 
 #else
 
-    #define log(X, Y)
-    #define log(X, Y, Z)
-    #define Err(X, Y)
-    #define Err(X, Y, Z)
+    #define log2(X, Y)
+    #define log3(X, Y, Z)
+    #define Err2(X, Y)
+    #define Err3(X, Y, Z)
 
 #endif
 
